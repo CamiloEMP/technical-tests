95aedc3c1f870b1bdc1cc2e14ba8d79c5cf64ec0
b7c8p <morapaezc@gmail.com>
2023-04-07 21:17:22 -0500

    Adding missing images

--- ehtt/src/App.tsx
+++ ehtt/src/App.tsx
@@ -1,11 +1,6 @@
-import people from './mocks/people.json'
-
 function App() {
   return (
     <div className="text-xl">
-      <div>
-        <img src={people[0]['company-image']} alt="" />
-      </div>
       <span>hola</span>
     </div>
   )
